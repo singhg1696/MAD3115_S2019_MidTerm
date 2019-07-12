3d45d4a1cb364e5f128a24da23ecccc717df6f32
Gurwinder SIngh <gurwinder958652@gmail.com>
2019-07-12 19:06:07 -0400

    [Gurwinder] TableViewCell Class and View created for displaying bill details for esch customer

---
+++ C0753140_MidTerm_MAD3115S2019/C0753140_MidTerm_MAD3115S2019/billDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  billDetailsTableViewCell.swift
+//  C0753140_MidTerm_MAD3115S2019
+//
+//  Created by Gurwinder on 2019-07-12.
+//  Copyright © 2019 Gurwinder. All rights reserved.
+//
+
+import UIKit
+
+class billDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
