a25a64b25dad2a80664a701e4fee3fe8913896ab
Gurwinder SIngh <gurwinder958652@gmail.com>
2019-07-12 19:40:16 -0400

    [Gurwinder] Worked on rest of the data ..

--- C0753140_MidTerm_MAD3115S2019/C0753140_MidTerm_MAD3115S2019/ShowBillDetailsViewController.swift
+++ C0753140_MidTerm_MAD3115S2019/C0753140_MidTerm_MAD3115S2019/ShowBillDetailsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ShowBillDetailsViewController: UIViewController {
 
+    @IBOutlet weak var tblBillDetails: UITableView!
     @IBOutlet weak var lblTotalBill: UILabel!
     @IBOutlet weak var lblCustomerEmail: UILabel!
     @IBOutlet weak var lblCustomerName: UILabel!
@@ -17,8 +18,8 @@ class ShowBillDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.tbl_billdetails.delegate = self
-        self.tbl_billdetails.dataSource = self
+//        self.tblBillDetails.delegate = self
+//        self.tblBillDetails.dataSource = self
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -42,18 +43,38 @@ class ShowBillDetailsViewController: UIViewController {
 
 }
 
-extension ShowBillDetailsViewController: UITableViewDataSource, UITableViewDelegate{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Customer.activeCustomer.billDictionary.count
-
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "idBill")  as! UITableViewCell
-        
-        let currentbill = Customer.activeCustomer.billDictionary[indexPath.row + 1]
-        var furtherDetails = ""
-    }
-    
-    
-}
+//extension ShowBillDetailsViewController: UITableViewDataSource, UITableViewDelegate
+//{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return Customer.activeCustomer.billDictionary.count
+//
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "billDetailsCell")  as! billDetailsTableViewCell
+//
+//        let currentbill = Customer.activeCustomer.billDictionary[indexPath.row + 1]
+//        var furtherDetails = ""
+//        if currentbill?.billType == billTypes.Mobile{
+//            cell.textLabel?.numberOfLines = 12
+//            let mobileBill = currentbill as! Mobile
+//            furtherDetails = "\nManufacturer Name : \(String(describing: mobileBill.mobileManufacturer)) \nPlan Name : \(String(describing: mobileBill.planName)) \nMobile Number : \(mobileBill.mobileNo) \nInternet used : \(String(describing: mobileBill.netUsed)) \nMinutes Used : \(String(describing: mobileBill.minuteUsed)) "
+//        } else {
+//            if currentbill?.billType == billTypes.Hydro{
+//                cell.textLabel?.numberOfLines = 12
+//                let hydroBill = currentbill as! Hydro
+//                furtherDetails = "Agency Name : \(String(describing: hydroBill.agencyName)) \n Unit Consumed : \(String(describing: hydroBill.unitsConsumed))"
+//            } else {
+//                if currentbill?.billType == billTypes.Internet{
+//                    cell.textLabel?.numberOfLines = 12
+//                    let internetBill = currentbill as! Internet
+//                    furtherDetails = "Provider Name : \(internetBill.providerName) \nInternet Used : \(String(describing: internetBill.internetUsed))"
+//                }
+//            }
+//        }
+//        cell.textLabel?.text = "Bill ID : \(String(describing: currentbill!.Id)) \nBill Date : \(String(describing: currentbill!.billDate)) \nBill Type : \(String(describing: currentbill!.billType)) \nBill Total : \(String(describing: currentbill!.totalBillAmount)) \(furtherDetails)"
+//        return cell
+//    }
+//
+//
+//}
